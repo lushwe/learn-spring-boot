8de22b14810171d44a6d5d68e323ca64c8511bbf
Jack Liu <947613622@qq.com>
2020-08-24 15:59:45 +0800

    redis increment 测试

--- spring-boot-data-redis/src/main/java/com/lushwe/redis/RedisBootstrap.java
+++ spring-boot-data-redis/src/main/java/com/lushwe/redis/RedisBootstrap.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
- * 说明：TODO 写点注释吧
+ * 说明：启动类
  *
  * @author Jack Liu
  * @date 2020-08-14 14:29
@@ -22,7 +22,9 @@ public class RedisBootstrap {
     private StringRedisTemplate stringRedisTemplate;
 
     public static void main(String[] args) {
+
         SpringApplication.run(RedisBootstrap.class, args);
+
     }
 
     @Component
@@ -30,25 +32,55 @@ public class RedisBootstrap {
 
         @Override
         public void run(ApplicationArguments args) throws Exception {
-            String key = "learn:redis:lushwe";
-            stringRedisTemplate.opsForValue().set(key, "Jack Liu");
-            String value = stringRedisTemplate.opsForValue().get(key);
-            System.out.println("value=" + value);
+            testGet();
+            testIncrement();
+            testBit();
         }
     }
 
-    private static void testBit() {
+    private void testGet() {
+
+        String key = "learn:redis:lushwe";
+
+        stringRedisTemplate.opsForValue().set(key, "Jack Liu");
+        String value = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("value=" + value);
+
+        // 删除
+        stringRedisTemplate.delete(key);
+    }
+
+    private void testIncrement() {
+
+        String key = "learn:redis:lushwe:in";
+
+        Long increment = null;
+        increment = stringRedisTemplate.opsForValue().increment(key);
+        System.out.println("increment=" + increment);
+        increment = stringRedisTemplate.opsForValue().increment(key);
+        System.out.println("increment=" + increment);
+
+        // 删除
+        stringRedisTemplate.delete(key);
+    }
+
+    private void testBit() {
+
+        String key = "sys:001";
 
         // bitmap
-//        Boolean setBit = stringRedisTemplate.opsForValue().setBit("sys:001", 100, true);
-//        System.out.println(setBit);
-//        Boolean bitResult = stringRedisTemplate.opsForValue().getBit("sys:001", 100);
-//        System.out.println(bitResult);
+        Boolean setBit = stringRedisTemplate.opsForValue().setBit(key, 100, true);
+        System.out.println(setBit);
+        Boolean bitResult = stringRedisTemplate.opsForValue().getBit(key, 100);
+        System.out.println(bitResult);
 
-//        String value = stringRedisTemplate.opsForValue().get("sys:001");
-//        System.out.println(value);
+        String value = stringRedisTemplate.opsForValue().get(key);
+        System.out.println(value);
 
-//        Long size = stringRedisTemplate.opsForValue().size("sys:001");
-//        System.out.println(size);
+        Long size = stringRedisTemplate.opsForValue().size(key);
+        System.out.println(size);
+
+        // 删除
+        stringRedisTemplate.delete(key);
     }
 }
