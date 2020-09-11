8ccf622983d80beb59f963e79a31b3749bd964f4
Jack Liu <947613622@qq.com>
2020-09-11 18:03:03 +0800

    统一处理返回结果格式，定义UserDTO对象

---
+++ spring-boot-project/src/main/java/com/lushwe/project/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.lushwe.project.dto;
+
+import lombok.Data;
+
+/**
+ * 说明：用户DTO
+ *
+ * @author Jack Liu
+ * @date 2020-09-11 17:23
+ * @since 0.1
+ */
+@Data
+public class UserDTO {
+
+    private Long userId;
+    private String userName;
+    private Integer age;
+}
