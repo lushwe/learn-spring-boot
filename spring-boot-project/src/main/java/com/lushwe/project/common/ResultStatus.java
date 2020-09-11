51eae7afce5d68a8452e5ac94c6e4b73481c9cd5
Jack Liu <947613622@qq.com>
2020-09-11 18:03:49 +0800

    统一处理返回结果格式，定义ResultStatus枚举

---
+++ spring-boot-project/src/main/java/com/lushwe/project/common/ResultStatus.java
@@ -0,0 +1,39 @@
+package com.lushwe.project.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 说明：状态码枚举类
+ *
+ * @author Jack Liu
+ * @date 2020-09-11 17:29
+ * @since 0.1
+ */
+@Getter
+public enum ResultStatus {
+
+    SUCCESS(HttpStatus.OK, 200, "OK"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "Bad Request"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal Server Error"),
+    ;
+
+    /**
+     * 返回的HTTP状态码,  符合http请求
+     */
+    private HttpStatus httpStatus;
+    /**
+     * 业务异常码
+     */
+    private Integer code;
+    /**
+     * 业务异常信息描述
+     */
+    private String message;
+
+    ResultStatus(HttpStatus httpStatus, Integer code, String message) {
+        this.httpStatus = httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+}
