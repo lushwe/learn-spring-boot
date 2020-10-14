28d6e4c73a5bcb83f2b547352a6e3ea4c4785abe
Jack Liu <947613622@qq.com>
2020-10-14 16:08:17 +0800

    事件监听调整为接口形式

--- spring-boot-event/src/main/java/com/lushwe/spring/boot/listener/ContextClosedEventListener.java
+++ spring-boot-event/src/main/java/com/lushwe/spring/boot/listener/ContextClosedEventListener.java
@@ -3,7 +3,6 @@ package com.lushwe.spring.boot.listener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
 
 /**
  * 说明：Spring 上下文关闭事件监听
@@ -12,23 +11,14 @@ import org.springframework.stereotype.Component;
  * @date 2020-06-20 10:07
  * @since 0.1
  */
-@Component
-public class ContextClosedEventListener {
+public interface ContextClosedEventListener {
 
+    /**
+     * 处理事件监听
+     *
+     * @param event
+     */
     @Async
     @EventListener
-    public void handleUserRegisterEvent(ContextClosedEvent event) {
-
-        System.out.println("监听事件: " + event);
-
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("事件处理完成");
-
-        System.out.println("当前监听对象: " + this);
-    }
+    void executeEvent(ContextClosedEvent event);
 }
