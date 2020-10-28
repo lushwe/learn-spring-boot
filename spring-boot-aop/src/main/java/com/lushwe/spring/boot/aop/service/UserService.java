a223aa5726f8e73a3dcd5b638b7f699493761de7
Jack Liu <947613622@qq.com>
2020-10-29 00:03:11 +0800

    业务测试Service

---
+++ spring-boot-aop/src/main/java/com/lushwe/spring/boot/aop/service/UserService.java
@@ -0,0 +1,43 @@
+package com.lushwe.spring.boot.aop.service;
+
+import com.lushwe.spring.boot.aop.domain.UserDTO;
+
+/**
+ * 说明：用户 Service 接口
+ *
+ * @author Jack Liu
+ * @date 2019-11-01 16:16
+ * @since 0.1
+ */
+public interface UserService {
+
+    /**
+     * 新增用户
+     *
+     * @param userDTO
+     */
+    void insertUser(UserDTO userDTO);
+
+    /**
+     * 更新用户
+     *
+     * @param userDTO
+     * @return
+     */
+    UserDTO updateUser(UserDTO userDTO);
+
+    /**
+     * 删除用户
+     *
+     * @param id
+     */
+    void deleteUser(Long id);
+
+    /**
+     * 根据用户ID查询用户信息
+     *
+     * @param userId
+     * @return
+     */
+    UserDTO findUserById(Long userId);
+}
