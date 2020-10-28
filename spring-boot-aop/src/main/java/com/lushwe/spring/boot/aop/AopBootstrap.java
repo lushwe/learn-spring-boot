cc7b44f2a98b590f0371d37ef01c5c5b0643761f
Jack Liu <947613622@qq.com>
2020-10-28 23:35:05 +0800

    spring-boot-aop项目启动类

---
+++ spring-boot-aop/src/main/java/com/lushwe/spring/boot/aop/AopBootstrap.java
@@ -0,0 +1,20 @@
+package com.lushwe.spring.boot.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 说明：启动类
+ *
+ * @author Jack Liu
+ * @date 2019-11-01 15:28
+ * @since 0.1
+ */
+@SpringBootApplication
+public class AopBootstrap {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AopBootstrap.class, args);
+    }
+}
